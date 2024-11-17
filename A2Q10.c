3eb6fcf9658577e7d8b68e97dcede6e9bafba1ef
PalakGupta6006 <24csu148@ncuindia.edu>
2024-11-17 22:42:31 +0530

    Add files via upload

--- A2Q10.c
+++ A2Q10.c
@@ -1,31 +1,4 @@
-/*#include<stdio.h>
-int main()
-{
-   int number[50],n,i,j,prime=0;
-   printf("Number of elements in array: ");
-   scanf("%d",&n);
-   for(i=0;i<n;i++)
-   {
-    scanf("%d",&number[i]);
-   }
-
-   for(j=0;j<number[j]/2;j++)
-   {
-    if(number[j]%j ==0)
-    {
-    printf("Number %d is composite",number[i]);
-    }
-    else if(((number[i] - 1)/2) % (j+1) == 0)
-    {
-      printf("%d is prime\n",number[i]);
-      prime++;
-    }
-   }
-   return 0;
-}*/
-
-
-
+// WAP to count prime numbers in an array.
 #include <stdio.h>
 #include <math.h>
 
@@ -39,7 +12,8 @@ int main() {
         scanf("%d", &number[i]);
     }
 
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         int num = number[i];
         if (num <= 1) {
             printf("%d is not prime\n", num);
@@ -60,8 +34,7 @@ int main() {
         } else {
             printf("%d is composite\n", num);
         }
-        printf("%d Prime numbers",prime);
     }
-
+          printf("%d Prime numbers",prime);
     return 0;
 }
